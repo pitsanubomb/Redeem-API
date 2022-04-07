9fcb186c346d50dcd80de2c3afa6385988b365f9
pitsanubomb <pitsanubomb@gmail.com>
2022-04-08 00:58:19 +0700

    :fire: Update connect db with user :stars:

--- src/users/interfaces/user.interface.ts
+++ src/users/interfaces/user.interface.ts
@@ -1,11 +1,11 @@
 export interface User {
-    id: number;
-    username: string;
-    firstName: string;
-    lastName: string;
-    point: number;
-    cash: number;
-    isActive: boolean;
-    createdAt: Date;
-    updatedAt: Date;
+  id: string;
+  username: string;
+  firstName: string;
+  lastName: string;
+  point: number;
+  cash: number;
+  isActive: boolean;
+  createdAt: Date;
+  updatedAt: Date;
 }
--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -1,11 +1,57 @@
-import { User } from "./interfaces/user.interface"
-import { PrismaClient } from '@prisma/client'
+import { User } from './interfaces/user.interface';
+import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient()
-const userRepo = prisma.user
+const prisma = new PrismaClient();
+const userRepo = prisma.user;
 
-export const findAllUsers = async (): Promise<any> => {
-    return userRepo.findMany()
-}
+export const findAllUsers = async (): Promise<User[]> => {
+  return userRepo.findMany({
+    select: {
+      id: true,
+      username: true,
+      firstName: true,
+      lastName: true,
+      point: true,
+      cash: true,
+      isActive: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
+};
 
+export const findUserById = async (id: string): Promise<User | null> => {
+  return userRepo.findUnique({
+    where: { id: id },
+    select: {
+      id: true,
+      username: true,
+      firstName: true,
+      lastName: true,
+      point: true,
+      cash: true,
+      isActive: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
+};
 
+export const authUser = async (body: {
+  email: string;
+  password: string;
+}): Promise<{ token: string } | undefined> => {
+  const query = {
+    where: {
+      email: body.email,
+    },
+    select: {
+      id: true,
+      email: true,
+      password: true,
+    },
+  };
+  const user: { id: string; email: string; password: string } | null =
+    await userRepo.findUnique(query);
+  if (user && user.password) return { token: 'xxxxxx' };
+};

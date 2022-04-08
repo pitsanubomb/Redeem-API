267262488a926d497f770e2a940775ea849caf9d
pitsanubomb <pitsanubomb@gmail.com>
2022-04-08 13:07:11 +0700

    :fire: User Flow Compleate :stars:

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,16 +1,33 @@
-import { Request, Response, Router } from 'express'
-import { User } from './interfaces/user.interface'
-import * as UserService from './user.service'
+import { Request, Response, Router } from 'express';
+import { User } from './interfaces/user.interface';
+import * as UserService from './user.service';
 
-
-export const UserController = Router()
+export const UserController = Router();
 
 // Get method
-UserController.get("/", async (req: Request, res: Response) => {
-    try {
-        const users: User[] = await UserService.findAllUsers()
-        res.status(200).send({ users: users })
-    } catch (error) {
-        res.status(500).send(error)
-    }
-})
+UserController.get('/', async (req: Request, res: Response) => {
+  try {
+    const users: User[] = await UserService.findAllUsers();
+    res.status(200).send({ users: users });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// Post method
+UserController.post('/', async (req: Request, res: Response) => {
+  try {
+    await UserService.createdUser(req.body);
+    res.status(201).send({ message: 'Add users success' });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// Auth method
+UserController.post('/login', async (req: Request, res: Response) => {
+  try {
+    const resService = await UserService.authUser(req.body);
+    res.status(201).send(resService);
+  } catch (error) {}
+});
